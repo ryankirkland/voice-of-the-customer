db57bbc1bd38c297e6818bd1d46399ae53364318
Ryan Kirkland <ryankirkland.py@gmail.com>
2020-09-19 19:29:32 -0600

    organize code and reviews into importable files and separate categories for multiple model dev

--- src/preprocess.py
+++ src/preprocess.py
@@ -12,66 +12,21 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 nltk.download('stopwords')
 
-def remove_punc(my_string:str) -> str:
-    '''Given a string, removes all punctuation and returned punctuation-less string'''
-    return re.sub(f'[{string.punctuation}]', '', my_string)
+stop_words = stopwords.words('english')
 
-def tokenize(str):
-    '''
-    Tokenize a str and return a tokenized list.
-    '''
-    return [word for word in word_tokenize(str)]
+def cleaned_reviews_dataframe(reviews_df):
+    reviews_df['title'] = reviews_df['title'].str.replace('\n', '')
+    reviews_df['desc'] = reviews_df['desc'].str.replace('\n','')
+    reviews_df['title_desc'] = reviews_df['title'] + reviews_df['desc']
+    if 'Unnamed: 0' in set(reviews_df.columns):
+        reviews_df = reviews_df.drop('Unnamed: 0', axis=1)
+    return reviews_df
 
-def lemmatize(doc):
-    '''Takes in a doc and lemmatizes tokens in doc
-    Parameters
-    ----------
-    doc: list of tokens
-    
-    Returns
-    -------
-    lemmatized tokens
-    '''
-    return [wordnet.lemmatize(tkn) for tkn in doc]
+def get_review_dates(df):
+    pass
 
-def rm_stop_words(doc, stops=set(stopwords.words('english'))):
-    '''Takes in a doc and removes stop words
-    Parameters
-    ----------
-    doc: list of tokens
-    
-    Returns
-    -------
-    Tokens with stop words removed
-    '''
-    return([w for w in doc if w not in stops])
+def docs_to_raw(df, colname):
+    return df[colname].tolist()
 
-def n_grams(input_tokens):
-    # retain 1-gram tokens
-    ret_list = list(input_tokens)
-    
-    for i in range(2,3):
-        # add each n-grams to the list
-        ret_list.extend(['-'.join(tgram) for tgram in ngrams(input_tokens, i)])
-    return(ret_list)
-    
-    
-def preprocess_corpus(content):
-    '''
-    Add docstring. Make flexible to allow for doing, or not doing, preprocessing functions. 
-    Parameters
-    ----------
-    content (str): a collection of strings
-    Returns
-    -------
-    A list of lists: each list contains a tokenized version of the original string
-    '''
-    preprocessed = []
-    for i in range(len(content)):
-        step_1 = remove_punc(content[i].lower())
-        step_2 = tokenize(step_1)
-        step_3 = lemmatize(step_2)
-        step_4 = rm_stop_words(step_3)
-        step_5 = n_grams(step_4)
-        preprocessed.append(step_5)
-    return preprocessed
+def vectorized_words(text_list, max_df=0.8, min_df=10, ngram_range=(1,3), stop_words=stop_words, extend_stopwords=False):
+    pass
